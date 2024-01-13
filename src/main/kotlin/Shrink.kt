ea53c04847a383132136881d2f91b5daa3203867
cneicy <im@crash.work>
2024-01-13 18:28:32 +0800

    节日特惠

--- src/main/kotlin/Shrink.kt
+++ src/main/kotlin/Shrink.kt
@@ -27,21 +27,38 @@ object Shrink : KotlinPlugin(
         version = "1.0",
     )
     { author("Eicy、Gary、Bingling、Lubang") }
-)
-{
+) {
     override fun onEnable() {
         logger.info { "ShrinkCore loaded" }
         val eventChannel = GlobalEventChannel.parentScope(this)
         eventChannel.subscribeAlways<GroupMessageEvent> {
-            val gID=group.id
+            val gID = group.id
             val twGr = 624487948L
             val gGr = 695610436L
-            //val lailiGr = 235231834L
-            val isTarget = gID==twGr||gID==gGr
+            val isTarget = gID == twGr || gID == gGr
+
+            val festivalMap = mapOf(
+                240209 to "除夕",
+                240210 to "春节",
+                240214 to "情人节",
+                240224 to "元宵节",
+                240404 to "清明节",
+                240501 to "劳动节",
+                240610 to "端午节",
+                240810 to "七夕节",
+                240917 to "中秋节",
+                241001 to "国庆节"
+            )
+
+            val today = SimpleDateFormat("yyMMdd").format(Date()).toInt()
+
+            val isFestival = festivalMap.keys.contains(today)
+
+
             if (message.contentToString() == "/jrrp") {
-                val senderID=this.sender.id
+                val senderID = this.sender.id
                 val random = Random(
-                    (SimpleDateFormat("yyMMdd").format(Date()).toInt() xor senderID.toInt()).toLong()
+                    (today xor senderID.toInt()).toLong()
                 )
                 val chain = buildMessageChain {
                     +At(senderID)
@@ -50,13 +67,14 @@ object Shrink : KotlinPlugin(
                 }
                 group.sendMessage(chain)
             }
-            if (message.contentToString() == "/ping"){
+            if (message.contentToString() == "/ping") {
                 group.sendMessage("Pong!")
             }
+
             if (message.contentToString() == "/r今日猝死率") {
-                val senderID=this.sender.id
+                val senderID = this.sender.id
                 val random = Random(
-                    (SimpleDateFormat("yyMMdd").format(Date()).toInt()+1 xor senderID.toInt()).toLong()
+                    (today + 1 xor senderID.toInt()).toLong()
                 )
                 val chain = buildMessageChain {
                     +At(senderID)
@@ -67,83 +85,92 @@ object Shrink : KotlinPlugin(
                 group.sendMessage(chain)
             }
 
-            if (message.contentToString() == "/modpacktoday?"||message.contentToString() == "/modpacktoday？"||message.contentToString() == "/modpacktoday") {
-                val senderID=this.sender.id
-                val random = Random(
-                    (SimpleDateFormat("yyMMdd").format(Date()).toInt() xor senderID.toInt()).toLong()
-                )
-                val corpus: Array<String> = arrayOf(
-                    " 你这辈子就只能做原生了。",
-                    " 天灵灵地灵灵，魔改大仙快显灵。",
-                    " 冒险包没有暮色，就像丁真没了雪豹。",
-                    " 适合头脑风暴，做包容易摆。",
-                    " 来点酷炫的实现！",
-                    "今天的人品值是: NaN",
-                    " 文档出没，大脑慢行。",
-                    " 只能搓一点点，不能搓多了。",
-                    " 魔改虽好，请不要贪杯哦。",
-                    " 空中有指针划过，谨防报错。",
-                    " /modpacktoday?",
-                    " 你看我整不整活就完事了。",
-                    " 草！(脚本报错)走！(进群问)忽略！(忘发pastebin)",
-                    " 做包时可能偶尔要看Doc。",
-                    " 做冒险包？考虑一下用数据包实现Boss技能吧！",
-                    " 整片天空都是CrT的颜色。",
-                    " 今日运势：凶，当心任务线绊倒人。",
-                    " 魔改一写、一Debug、一天就过去了哈~",
-                    " 宜:魔改。",
-                    " 谨防配方伤人事件。",
-                    " 试试搓个库吧。",
-                    " 小 心 迷 信",
-                    " 最讨厌的两种人，一种是报错发群不发全，另一种是",
-                    " 可以放开手脚地搓！",
-                    " 谨防伪劣魔改伤人。",
-                    " 今天玩玩自己的包？",
-                    " 友谊护佑，今日代码出错概率降低。",
-                    " 超级魔改人！",
-                    " 在帮助玩家解决问题时你发现TA电脑里有二十多个Java",
-                    " 恐怖魔改人！",
-                    " ---CLEANROOM---",
-                    " 今日运势：吉，艺术细胞复苏；宜做材质，忌摆配方。",
-                    " 吾日三省吾身，看文档了没，拜友谊了没，语法检查了没。",
-                    " 有没有考虑过自己玩自己的整合包?",
-                    " 小改怡情，大改伤身，全改鸽子纷飞。",
-                    " 今日在scripts路出现了一起分号失踪事件，造成三个配方报错，两位大佬升压，请各位魔改人保管好自己的分号。",
-                    " 魔改千万条，安全第一条；代码不规范，友谊两行泪。",
-                    " 魔改千万条，安全第一条；数值不规范，玩家泪两行。",
-                    " 谨防GT污染!!!",
-                    " 前途一片光明啊！（赞赏",
-                    " 本条语料由格雷科技无限公司赞助!",
-                    " 本条语料由沉浸重工友情冠名！",
-                    " 本条语料由热力建筑倾情赞助",
-                    " 本条语料由末影接口独家代言",
-                    " 虫子出没，谨防报错!",
-                    " 伤肝事小，费时事大，做包适度！",
-                    " 今日在minecraft街道出现一起魔改伤人事件，当事整合包咬伤过路行人肝五个，现已被魔改规范局逮捕！希望各位玩家和作者谨防大型整合包伤人。",
-                    " 你看那天上的烟花像不像你的报错 五花八门~",
-                    " 你的整合包已经被我reika的替身【瞬间崩溃】摸过了！",
-                    " 报错就像海绵里的水，只要你肯挤，总会出来的。",
-                    " 行",
-                    " 做整合包哪有不疯的，硬撑罢了！",
-                    " 今日已控制数名意欲行刺整合包作者的玩家，他们被捕前大喊反对赛博徭役口号。",
-                    " 不行"
+            if (message.contentToString() == "/modpacktoday?" || message.contentToString() == "/modpacktoday？" || message.contentToString() == "/modpacktoday") {
+                val senderID = this.sender.id
+                if (isFestival) {
+                    val chain = buildMessageChain {
+                        +At(senderID)
+                        +PlainText("隔壁叔叔"+festivalMap.get(today)+"还在做魔改，你长大千万别像他那样。")
+                    }
+                    group.sendMessage(chain)
+                } else {
+                    val random = Random(
+                        (today xor senderID.toInt()).toLong()
                     )
-                val answer: String?
-                val randInt = random.nextInt(corpus.size)
-                answer = corpus[randInt]
+                    val corpus: Array<String> = arrayOf(
+                        " 你这辈子就只能做原生了。",
+                        " 天灵灵地灵灵，魔改大仙快显灵。",
+                        " 冒险包没有暮色，就像丁真没了雪豹。",
+                        " 适合头脑风暴，做包容易摆。",
+                        " 没有日志我能做的事只有帮你算一卦。",
+                        " 来点酷炫的实现！",
+                        "今天的人品值是: NaN",
+                        " 文档出没，大脑慢行。",
+                        " 只能搓一点点，不能搓多了。",
+                        " 魔改虽好，请不要贪杯哦。",
+                        " 空中有指针划过，谨防报错。",
+                        " /modpacktoday?",
+                        " 你看我整不整活就完事了。",
+                        " 草！(脚本报错)走！(进群问)忽略！(忘发pastebin)",
+                        " 做包时可能偶尔要看Doc。",
+                        " 做冒险包？考虑一下用数据包实现Boss技能吧！",
+                        " 整片天空都是CrT的颜色。",
+                        " 今日运势：凶，当心任务线绊倒人。",
+                        " 魔改一写、一Debug、一天就过去了哈~",
+                        " 宜:魔改。",
+                        " 谨防配方伤人事件。",
+                        " 试试搓个库吧。",
+                        " 小 心 迷 信",
+                        " 最讨厌的两种人，一种是报错发群不发全，另一种是",
+                        " 可以放开手脚地搓！",
+                        " 谨防伪劣魔改伤人。",
+                        " 今天玩玩自己的包？",
+                        " 友谊护佑，今日代码出错概率降低。",
+                        " 超级魔改人！",
+                        " 在帮助玩家解决问题时你发现TA电脑里有二十多个Java",
+                        " 恐怖魔改人！",
+                        " ---CLEANROOM---",
+                        " 今日运势：吉，艺术细胞复苏；宜做材质，忌摆配方。",
+                        " 吾日三省吾身，看文档了没，拜友谊了没，语法检查了没。",
+                        " 有没有考虑过自己玩自己的整合包?",
+                        " 小改怡情，大改伤身，全改鸽子纷飞。",
+                        " 今日在scripts路出现了一起分号失踪事件，造成三个配方报错，两位大佬升压，请各位魔改人保管好自己的分号。",
+                        " 魔改千万条，安全第一条；代码不规范，友谊两行泪。",
+                        " 魔改千万条，安全第一条；数值不规范，玩家泪两行。",
+                        " 谨防GT污染!!!",
+                        " 前途一片光明啊！（赞赏",
+                        " 本条语料由格雷科技无限公司赞助!",
+                        " 本条语料由沉浸重工友情冠名！",
+                        " 本条语料由热力建筑倾情赞助",
+                        " 本条语料由末影接口独家代言",
+                        " 虫子出没，谨防报错!",
+                        " 伤肝事小，费时事大，做包适度！",
+                        " 今日在minecraft街道出现一起魔改伤人事件，当事整合包咬伤过路行人肝五个，现已被魔改规范局逮捕！希望各位玩家和作者谨防大型整合包伤人。",
+                        " 你看那天上的烟花像不像你的报错 五花八门~",
+                        " 你的整合包已经被我reika的替身【瞬间崩溃】摸过了！",
+                        " 报错就像海绵里的水，只要你肯挤，总会出来的。",
+                        " 行",
+                        " 做整合包哪有不疯的，硬撑罢了！",
+                        " 今日已控制数名意欲行刺整合包作者的玩家，他们被捕前大喊反对赛博徭役口号。",
+                        " 不行"
+                    )
+                    val answer: String?
+                    val randInt = random.nextInt(corpus.size)
+                    answer = corpus[randInt]
 
-                val chain = buildMessageChain {
-                    +At(senderID)
-                    +PlainText(answer)
+                    val chain = buildMessageChain {
+                        +At(senderID)
+                        +PlainText(answer)
+                    }
+                    group.sendMessage(chain)
                 }
-                group.sendMessage(chain)
             }
 
-            if(isTarget){
-                when(message.contentToString()){
+            if (isTarget) {
+                when (message.contentToString()) {
                     "/help" ->
-group.sendMessage(
-"""指令一览：
+                        group.sendMessage(
+                            """指令一览：
 /ping Bot存活测试
 /info 显示Bot资料
 /help 显示Bot帮助信息
@@ -157,33 +184,37 @@ group.sendMessage(
 /mtcmds MineTweaker 部分常用指令使用方法
 /whyvsc 为什么要使用 VisualStudio Code ？
 /links 实用链接"""
-)
+                        )
+
                     "/info" ->
-group.sendMessage(
-"""Shrink是由Eicy为Minecraft魔改交流群部署的QQ bot。
+                        group.sendMessage(
+                            """Shrink是由Eicy为Minecraft魔改交流群部署的QQ bot。
 语料和部分插件代码来自
 BakaHDT(https://github.com/bingling-sama/BakaHDT)
 ChlorineSharp(https://github.com/GBLodb/ChlorineSharp)
 Shrink(https://github.com/cneicy/Shrink)"""
-)
+                        )
+
                     "/ask" ->
-group.sendMessage(
-"""提问的正确方式：
+                        group.sendMessage(
+                            """提问的正确方式：
 简明清晰地描述你的游戏版本&遇到的问题
 魔改问题请将[crafttweaker.log]和log中提到/你认为可能出错的代码使用pastebin发送到群内
 (详见/pastebin)"""
-)
+                        )
+
                     "/pastebin" ->
-group.sendMessage(
-"""mclo.gs使用介绍：
+                        group.sendMessage(
+                            """mclo.gs使用介绍：
 1.打开https://mclo.gs/
 2.上传日志文件或将日志内容复制到框内
 3.点击Save
 4.复制网址栏网址发送至群内"""
-)
+                        )
+
                     "/logs" ->
-group.sendMessage(
-"""常见常用日志文件类型/路径:
+                        group.sendMessage(
+                            """常见常用日志文件类型/路径:
 崩溃报告(cr) - .minecraft/crash-reports/crash-xxx.log
 崩溃报告可能有多份, 请发送时间最近的。
 游戏日志(latest.log) - .minecraft/logs/latest.log
@@ -191,10 +222,11 @@ group.sendMessage(
 CraftTweaker日志文件(crtlog) - .minecraft/crafttweaker.log
 MineTweaker日志文件(mtlog) - .minecraft/minetweaker.log
 若需分享日志供他人分析请使用命令/pastebin"""
-)
+                        )
+
                     "/rules" ->
-group.sendMessage(
-"""《Minecraft魔改交流群 群规》请熟读并背诵
+                        group.sendMessage(
+                            """《Minecraft魔改交流群 群规》请熟读并背诵
 1.不允许多次加群, 超过[3次]将被加入本群黑名单;
 2.禁止以任何形式发布有关 [键政/商业广告/非私人服务器宣传] 的消息;
 3.禁止以任何形式发布R18等[限制级]内容, 擦边球也不行;
@@ -206,20 +238,22 @@ group.sendMessage(
 9.仅在不影响他人的前提下讨论非MC内容/非魔改内容;
 10.发消息尤其是问问题, 经过思考整理好语言再发, 且尽量不要刷屏式发消息;
 *本消息可能不会被及时更新, 最新群规内容永远以群公告为准*"""
-)
+                        )
+
                     "/crtcmds" ->
-group.sendMessage(
-"""CraftTweaker 可用的部分实用指令：
+                        group.sendMessage(
+                            """CraftTweaker 可用的部分实用指令：
 /ct hand 输出玩家手上物品的 ID / 矿辞 (1.12-) / 标签 (1.14+) 等信息，你可以点击有关信息将其复制到剪贴板里。
 /ct syntax 检查脚本的语法是否准确。注意语法正确也不代表一定能运行的符合预期，此指令仅能检查语法问题，无法检查逻辑问题。
 /ct log 打开 CrT 日志文件，请配合 &pastebin 发送。
 /ct inventory 输出玩家物品栏的所有物品的 ID。
 /ct conflict 打印所有冲突配方和其总数。 (仅限工作台和熔炉)
 /reload (1.14+ 可用) 重载脚本。但 CoT 脚本不可重载。"""
-)
+                        )
+
                     "/mtcmds" ->
-group.sendMessage(
-"""MineTweaker 可用的部分实用指令: 
+                        group.sendMessage(
+                            """MineTweaker 可用的部分实用指令: 
 /mt hand 输出玩家手上物品的 ID 等信息，你可以点击有关信息将其复制到剪贴板里。
 /mt inventory 输出玩家物品栏的所有物品的 ID。
 /mt oredict 打印所有游戏内存在的矿辞。
@@ -227,10 +261,11 @@ group.sendMessage(
 /mt blocks 可以输出游戏内所有方块的注册ID。
 /mt blockinfo 开启时左右键可查看方块数据，再次输入指令以关闭。
 /mt reload 重载脚本。"""
-)
+                        )
+
                     "/whyvsc" ->
-group.sendMessage(
-"""VisualStudio Code(以下简称VSCode) 是当下编写 ZenScript 代码的最佳选择之一, 也是本群极力推荐的 文本编辑工具。
+                        group.sendMessage(
+                            """VisualStudio Code(以下简称VSCode) 是当下编写 ZenScript 代码的最佳选择之一, 也是本群极力推荐的 文本编辑工具。
 为什么? 请花半分钟阅读以下理由:
 VSCode 是一款由微软维护的, 功能十分丰富, 用户社区和插件社区极其发达的文本编辑器。
 对于 ZenScript 这种脚本语言, 是最佳的编写工具之一, 安装了插件后, 可以提供代码高亮和语法纠错, NBT 预览和编辑, Language 文件高亮等等功能。
@@ -241,10 +276,11 @@ VSCode 是一款由微软维护的, 功能十分丰富, 用户社区和插件社
 同时你可以使用 ProbeZS 模组将所有 ZenScript 类、方法，以及原版尖括号引用的内容导出成 dzs 文件搭配Visual Studio Code的 Zenscript Intelli Sense 插件提高Zenscript编写效率。
 ProbeZS:https://github.com/friendlyhj/ProbeZS
 Zenscript Intelli Sense:https://github.com/raylras/zenscript-intelli-sense"""
-)
+                        )
+
                     "/links" ->
-group.sendMessage(
-"""实用链接:
+                        group.sendMessage(
+                            """实用链接:
 CraftTweaker 官方文档: https://docs.blamejared.com/
 MineTweaker 官方文档: http://minetweaker3.powerofbytes.com/wiki/Main_Page/
 TweakerGroup Discord: https://discord.gg/4XsUtUfDFt/
@@ -255,15 +291,15 @@ CurseForge Minecraft Mods: https://www.curseforge.com/minecraft/mc-mods/
 ZenTutorial: https://youyi580.gitbook.io/zentutorial/
 Cog: https://youyi580.gitbook.io/cog/
 UsefulMods: https://github.com/TheUsefulLists/UsefulMods"""
-)
+                        )
                 }
             }
         }
         eventChannel.subscribeAlways<MemberJoinEvent> {
             val groupID = group.id
-            if(groupID == 624487948L||groupID == 214558144L){
+            if (groupID == 624487948L || groupID == 214558144L) {
                 group.sendMessage(
-"""欢迎加入Minecraft魔改交流群，进群请先阅读所有置顶公告。提问请携带尽可能多的相关信息
+                    """欢迎加入Minecraft魔改交流群，进群请先阅读所有置顶公告。提问请携带尽可能多的相关信息
 Discord群：https://discord.gg/sB9PhGcutE/
 CRT等魔改类模组错误还需附带脚本内容和输出LOG。
 详见/ask/pastebin/log
